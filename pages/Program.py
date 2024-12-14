50f994f434a67d62fe246cf09bd016651060c396
Rudy <rudy.hariyanto@student.president.ac.id>
2024-12-14 18:24:20 +0700

    Update Program.py

--- pages/Program.py
+++ pages/Program.py
@@ -10,6 +10,7 @@ def select_image_file():
         filetypes=[("Image Files", ".png;.jpeg;*.jpg")],
         title="Select an Image File"
     )
+     return file-patch
 
 # Function to apply rotation
 def rotate_image(image, angle):
