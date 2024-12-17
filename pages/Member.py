812d3193978cf66054d81c1bdcbc029e8046f52b
Rudy <rudy.hariyanto@student.president.ac.id>
2024-12-17 11:48:42 +0700

    Update Member.py

--- pages/Member.py
+++ pages/Member.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 st.subheader('Member:')
 st.text('Syaeful Imam NIM 004202305019')
-st.image('https://github.com/rudyh-04/image_processing/blob/main/avatar.jpg')
+st.image('avatar.jpg')
 
 st.text('Rudy Hariyanto NIM 004202405056')
 st.image('rudy.jpg')
